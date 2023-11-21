c056632161f0e308bf002ab4b85daba9a7555816
Hasan Armoush <hkarmoush@gmail.com>
2023-11-22 00:22:10 +0300

    network service
    readability

--- lib/core/services/network_service.dart
+++ lib/core/services/network_service.dart
@@ -2,64 +2,81 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:old/core/services/i_network_service.dart';
 
+typedef Headers = Map<String, dynamic>;
+typedef JSON = Map<String, dynamic>;
+
 class NetworkService extends INetworkService {
-  NetworkService({required super.baseUrl, required super.key});
+  NetworkService({
+    required super.baseUrl,
+    required super.key,
+    super.defaultHeaders,
+  });
 
   @override
   Future<T> request<T>(
     String endpoint, {
     required HttpMethods method,
-    Map<String, dynamic>? headers,
+    Headers? headers,
   }) async {
     final uri = '${super.baseUrl}/$endpoint';
+    final mergedHeaders = _mergeHeaders(defaultHeaders, headers);
+
     switch (method) {
       case HttpMethods.get:
-        return _get(uri);
+        return _get(uri, headers: mergedHeaders);
       case HttpMethods.post:
-        return _post(uri);
+        return _post(uri, headers: mergedHeaders);
       case HttpMethods.put:
-        return _put(uri);
+        return _put(uri, headers: mergedHeaders);
       case HttpMethods.delete:
-        return _delete(uri);
+        return _delete(uri, headers: mergedHeaders);
     }
   }
 
-  Future<T> _get<T>(String uri) async {
+  Future<T> _get<T>(String uri, {Headers? headers}) async {
     final response = await http.get(
       Uri.dataFromString(uri),
     );
     return _parseResponse<T>(response.body);
   }
 
-  Future<T> _post<T>(String uri) async {
+  Future<T> _post<T>(String uri, {Headers? headers}) async {
     final response = await http.post(
       Uri.dataFromString(uri),
     );
     return _parseResponse<T>(response.body);
   }
 
-  Future<T> _put<T>(String uri) async {
+  Future<T> _put<T>(String uri, {Headers? headers}) async {
     final response = await http.put(
       Uri.dataFromString(uri),
     );
     return _parseResponse<T>(response.body);
   }
 
-  Future<T> _delete<T>(String uri) async {
+  Future<T> _delete<T>(String uri, {Headers? headers}) async {
     final response = await http.delete(
       Uri.dataFromString(uri),
     );
     return _parseResponse<T>(response.body);
   }
 
+  Headers? _mergeHeaders(
+    Headers? defaultHeaders,
+    Headers? customHeaders,
+  ) {
+    final mergedHeaders = {...?defaultHeaders, ...?customHeaders};
+    return mergedHeaders.isNotEmpty ? mergedHeaders : null;
+  }
+
   T _parseResponse<T>(String responseBody) {
     if (T == String) {
       return responseBody as T;
-    } else if (T == Map<String, dynamic>) {
+    } else if (T == JSON) {
       return json.decode(responseBody) as T;
-    } else if (T == List<Map<String, dynamic>>) {
+    } else if (T == List<JSON>) {
       final decodedList = json.decode(responseBody) as Iterable<dynamic>;
-      return List<Map<String, dynamic>>.from(decodedList) as T;
+      return List<JSON>.from(decodedList) as T;
     } else {
       throw Exception('Unsupported type: $T');
     }
